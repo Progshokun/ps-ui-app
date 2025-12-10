30d5fa288290fd613e5a4175015d82623a77bc41
Oleg Lapin <pankiller11@gmail.com>
2025-12-10 12:42:30 +0300

    add type for children

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,18 +1,23 @@
 import Header from "./layout/Header/Header";
 import Footer from "./layout/Footer/Footer";
 import { useGameStore } from "./store";
+import type { ReactNode } from "react";
 
-const Layout = ({ children }) => {
+interface IProps {
+  children: ReactNode; // Это стандартный тип для любых React-элементов
+}
+
+const Layout = ({ children }: IProps) => {
   const { getActiveGame } = useGameStore();
 
   return (
     <div
-      className="bg-cover bg-no-repeat bg-center h-screen relative"
+      className="relative bg-cover bg-no-repeat bg-center h-screen"
       style={{
         backgroundImage: `url(${getActiveGame()?.bgImage})`,
       }}
     >
-      <div className="absolute inset-0 pointer-events-none bg-[radial-gradient(circle,rgba(0,0,0,0)_40%,rgba(0,0,0,0.75)_100%)]" />
+      <div className="absolute inset-0 bg-[radial-gradient(circle,rgba(0,0,0,0)_40%,rgba(0,0,0,0.75)_100%)] pointer-events-none" />
       <div className="relative h-full">
         <Header />
         <main>{children}</main>
