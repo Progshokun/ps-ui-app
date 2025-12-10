0358122ac8931adcbdee62dea94c45cd43ddfddb
Oleg Lapin <pankiller11@gmail.com>
2025-12-10 12:26:15 +0300

    add preview

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,9 +1,9 @@
 import { hotkeysData } from "../../data/hotkeys.data";
-import KeyInfo from "../../ui/keyInfo";
+import KeyInfo from "../../ui/KeyInfo";
 
 const Footer = () => {
   return (
-    <footer className="flex items-center gap-5 absolute bottom-5 right-8 text-sm">
+    <footer className="right-8 bottom-5 absolute flex items-center gap-5 text-sm">
       {hotkeysData.map((hotKey) => (
         <KeyInfo key={hotKey.label} keyInfo={hotKey} />
       ))}
